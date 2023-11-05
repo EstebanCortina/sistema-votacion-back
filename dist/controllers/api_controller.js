46f70888880beed098458d2d7c16107223810e38
Esteban Cortina <estebannsebastian@gmail.com>
2023-11-05 12:24:25 -0600

    Add api controller

---
+++ dist/controllers/api_controller.js
@@ -0,0 +1,7 @@
+var Api_controller;
+(function (Api_controller) {
+    module.exports = (req, res) => {
+        res.status(200).send("controller");
+    };
+})(Api_controller || (Api_controller = {}));
+;
