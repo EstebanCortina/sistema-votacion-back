9ba8bd637aca37f265e7bf3f1be197c40a80506c
Esteban Cortina <estebannsebastian@gmail.com>
2023-11-05 20:27:42 -0600

    console log

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,6 @@
 const app = require('./config/express_config.js');
 const morgan = require('morgan');
 const { MORGAN_ENV, NODE_ENV, PORT } = require('./env_variables.js');
-console.log(MORGAN_ENV);
 app.use(morgan(MORGAN_ENV));
 const router = require('./routes/index.js');
 app.use('/', router);
