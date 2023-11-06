a310715ee53190636d9880ed9c3b4026a69f0341
Esteban Cortina <estebannsebastian@gmail.com>
2023-11-05 20:58:31 -0600

    Add node env logger

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,14 @@
 const app = require('./config/express_config.js');
 const morgan = require('morgan');
 const { MORGAN_ENV, NODE_ENV, PORT } = require('./env_variables.js');
+function customLogger(message) {
+    return (req, res, next) => {
+        console.log(message); // Agrega tu mensaje personalizado aquí
+        next();
+    };
+}
+// Utiliza el middleware personalizado antes de morgan para agregar un mensaje
+app.use(customLogger(NODE_ENV));
 app.use(morgan(MORGAN_ENV));
 const router = require('./routes/index.js');
 app.use('/', router);
