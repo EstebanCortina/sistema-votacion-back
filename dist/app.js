3deb77431456f07e033f11378a0fe1ba623d865a
Esteban Cortina <estebannsebastian@gmail.com>
2023-11-08 21:28:59 -0600

    Second eslint correction & prettier

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
-const { app } = require("./config/express_config");
-const morgan = require("morgan");
-const { MORGAN_ENV, NODE_ENV, PORT } = require("./env_variables");
+const { app } = require('./config/express_config');
+const morgan = require('morgan');
+const { MORGAN_ENV, NODE_ENV, PORT } = require('./env_variables');
 function customLogger(message) {
     return (req, res, next) => {
         console.log(message); // Agrega tu mensaje personalizado aquí
@@ -10,8 +10,8 @@ function customLogger(message) {
 // Utiliza el middleware personalizado antes de morgan para agregar un mensaje
 app.use(customLogger(NODE_ENV));
 app.use(morgan(MORGAN_ENV));
-const router = require("./routes/index");
-app.use("/", router);
+const router = require('./routes/index');
+app.use('/', router);
 app.listen(PORT, () => {
     console.log(`[${NODE_ENV}]Running on ${PORT}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-const { app } = require("./config/express_config");
-const morgan = require("morgan");
-const { MORGAN_ENV, NODE_ENV, PORT } = require("./env_variables");
+const { app } = require('./config/express_config');
+const morgan = require('morgan');
+const { MORGAN_ENV, NODE_ENV, PORT } = require('./env_variables');
 
 function customLogger(message: string) {
   return (req: Request, res: Response, next) => {
@@ -14,8 +14,8 @@ app.use(customLogger(NODE_ENV));
 
 app.use(morgan(MORGAN_ENV));
 
-const router = require("./routes/index");
-app.use("/", router);
+const router = require('./routes/index');
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`[${NODE_ENV}]Running on ${PORT}`);
