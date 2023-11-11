2f347f43666bb5498d52332bc8b8dc28dcfb00cd
Esteban Cortina <estebannsebastian@gmail.com>
2023-11-11 14:47:55 -0600

    Add typescript routes & tsc-alias

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 const expressRouter = require('express').Router();
-const { verifyToken } = require('../middleware/jwt');
+const { verifyToken } = require('@middleware/jwt');
 expressRouter.get('/', (req, res): void => {
   res.status(200).send('<h1>Ruta</h1>');
 });
