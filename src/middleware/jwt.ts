2e0c651305649db4bd8485bb7b32f2a4199fb766
Esteban Cortina <estebannsebastian@gmail.com>
2023-11-11 01:53:30 -0600

    Auth api route with jwt

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -2,7 +2,8 @@ module.exports = {
   verifyToken: (req, res, next) => {
     const { SECRET_KEY } = require('../env_variables');
     const jwt = require('jsonwebtoken');
-    const token = req.body.token;
+    const token =
+      req.headers.authorization && req.headers.authorization.split(' ')[1];
 
     if (!token) {
       return res.status(403).json({ message: 'Token no proporcionado' });
