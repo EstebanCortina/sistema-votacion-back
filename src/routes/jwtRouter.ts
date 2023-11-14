5fbd1700976a4579515c7c2b62353bf515ccf307
Esteban Cortina <estebannsebastian@gmail.com>
2023-11-13 19:52:20 -0600

    LF

--- src/routes/jwtRouter.ts
+++ src/routes/jwtRouter.ts
@@ -1,6 +1,5 @@
 module.exports = (() => {
   const router = require('express').Router();
-
   const jwtController = require('../controllers/jwtController');
   router.get('/', jwtController);
   return router;
